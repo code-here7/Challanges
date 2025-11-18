7a3cbe97b04e75bc2bfc3a7adc4c5c947cb53583
code-here7 <yashasviny@gmail.com>
2025-11-18 14:56:33 +0530

    Adding Fetch API data app Improved code...with loading and custom error try catch

--- react challanges/react-evolution/Intermediate/vite-fetch-api-render/src/components/FetchData.jsx
+++ react challanges/react-evolution/Intermediate/vite-fetch-api-render/src/components/FetchData.jsx
@@ -2,24 +2,40 @@ import {  useEffect, useState } from "react";
 
 const FetchData = () => {
     const [users,setUsers] = useState([]);
+    const [loading,setLoading] =  useState(true);
+    const [err,setErr] = useState(null);
 
     useEffect(() => {
     const usersFetch = async () => {
+        try{
      const result = await fetch("https://jsonplaceholder.typicode.com/users");
+     if (!result.ok) throw new Error("Some issue found"); //custom error
      const res = await result.json(); 
      console.log(res); //upar wait nh lagega to console pehle handle ho jayega aur tab 
      // tak promise handle nahi ho paega.
      setUsers(res);
+        }
+        catch(e)
+        {
+        //   console.log(e)
+        setErr(e.message);
+        }
+        finally
+        {
+         setLoading(false);
+        }
     }
     usersFetch();
     },[]);
     const containerStyle = {border:"2px solid black",padding: "0 10%"};
-
+  
+    if(loading) return <h2>The Page is in Progress</h2>
+    if(err) return <h2>{err}</h2>
     return(
         <>
        <div style={containerStyle}>
          <h1>Users Data</h1>
-        {
+        { users && 
             users.map((item)=> 
              <ul key={item.id}>
             <li>{item.name}</li>
